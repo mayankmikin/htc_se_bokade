1ebb939520074cbd070a04b1ea16f439cdc789cd
Mayank Verma <mayank.mikin@gmail.com>
2020-04-04 20:15:00 +0200

    +++

--- src/app/pages/qr/qr.component.ts
+++ src/app/pages/qr/qr.component.ts
@@ -11,5 +11,24 @@ export class QrComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  qrcodename : string;
+  title = 'generate-qrcode';
+  elementType: 'url' | 'canvas' | 'img' = 'url';
+  value: string;
+  display = false;
+  href : string;
+  generateQRCode(){
+    if(this.qrcodename == ''){
+      this.display = false;
+      alert("Please enter the details");
+      return;
+    }
+    else{
+      this.value = this.qrcodename;
+      this.display = true;
+    }
+  }
+  downloadImage(){
+    this.href = document.getElementsByTagName('img')[0].src;
+  }
 }
